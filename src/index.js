8343cc9007b9f37b2d96cfe6a8e5fbf18a54390b
Mudit Agarwal <mudit.agar@gmail.com>
2022-05-10 11:59:55 +0530

    updated server url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://blooming-savannah-86282.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
